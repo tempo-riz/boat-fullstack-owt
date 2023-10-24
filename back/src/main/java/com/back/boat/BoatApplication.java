d3410f67e2f7e9782f44d7bc391d56259fa1663a
Thibault <thibaultchatillon@gmail.com>
2023-10-24 12:58:04 +0200

    moved data in back

--- back/src/main/java/com/back/boat/BoatApplication.java
+++ back/src/main/java/com/back/boat/BoatApplication.java
@@ -2,14 +2,14 @@ package com.back.boat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BoatApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BoatApplication.class, args);
+
+    System.out.println("\nAPI up and running at http://localhost:8080/boats");
   }
 
 }
